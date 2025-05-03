001da49e71f51599c6ef719e8de7cbd90684d60a
jahn <ahn.jeff@gmail.com>
2025-05-02 17:34:35 -0700

    add random diffuse vector code

--- src/InOneWeekend/sphere.cc
+++ src/InOneWeekend/sphere.cc
@@ -44,3 +44,13 @@ bool Sphere::hit(const Ray& ray,
 
   return true;
 }
+
+// static
+Vec3 Sphere::random_vec3_on_surface(const Vec3& surface_norm) {
+  Vec3 random_unit_vec = Vec3::random_unit_vec3();
+  if (random_unit_vec.dot(surface_norm) > 0) {
+    return random_unit_vec;
+  } else {
+    return -random_unit_vec;
+  }
+}
--- src/InOneWeekend/sphere.h
+++ src/InOneWeekend/sphere.h
@@ -4,6 +4,7 @@
 #include "hittable.h"
 #include "interval.h"
 #include "point3.h"
+#include "vec3.h"
 
 class Ray;
 
@@ -11,6 +12,9 @@ class Sphere : public Hittable {
  public:
   Sphere(Point3, double);
   ~Sphere() override;
+
+  // Returns a random unit vector in the same direction as |surface_norm|
+  static Vec3 random_vec3_on_surface(const Vec3& surface_norm);
 
   bool hit(const Ray& ray, Interval intval, HitResult& result) const override;
 

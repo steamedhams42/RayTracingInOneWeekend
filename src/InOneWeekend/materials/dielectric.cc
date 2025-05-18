c21c71bc148879f7cbab4b048774d77a109fc338
Jeff Ahn <ahn.jeff@gmail.com>
2025-05-17 19:11:26 -0700

    use wikipedia's formula for refraction

--- src/InOneWeekend/materials/dielectric.cc
+++ src/InOneWeekend/materials/dielectric.cc
@@ -9,20 +9,6 @@
 Dielectric::Dielectric(const double refraction_index)
     : refraction_index_(refraction_index) {}
 
-Vec3 Dielectric::refract(const Vec3& uv,
-                         const Vec3& n,
-                         double etai_over_etat) const {
-  // auto cos_theta = std::fmin(-uv.dot(n), 1.0);
-  // Vec3 r_out_perp = etai_over_etat * (uv + cos_theta * n);
-  // Vec3 r_out_parallel =
-  //     -std::sqrt(std::fabs(1.0 - r_out_perp.norm_squared())) * n;
-  // return r_out_perp + r_out_parallel;
-
-  double r = etai_over_etat;
-  double c = -n.dot(uv);
-  return r * uv + (r * c - std::sqrt(1 - r * r * (1 - c * c))) * n;
-}
-
 bool Dielectric::scatter(const Ray& incident_ray,
                          const Hittable::HitResult& result,
                          Color& attenuation,
@@ -35,4 +21,18 @@ bool Dielectric::scatter(const Ray& incident_ray,
 
   scattered = Ray(result.p, refracted);
   return true;
+}
+
+Vec3 Dielectric::refract(const Vec3& uv,
+                         const Vec3& n,
+                         double etai_over_etat) const {
+  // auto cos_theta = std::fmin(-uv.dot(n), 1.0);
+  // Vec3 r_out_perp = etai_over_etat * (uv + cos_theta * n);
+  // Vec3 r_out_parallel =
+  //     -std::sqrt(std::fabs(1.0 - r_out_perp.norm_squared())) * n;
+  // return r_out_perp + r_out_parallel;
+
+  double r = etai_over_etat;
+  double c = -n.dot(uv);
+  return r * uv + (r * c - std::sqrt(1 - r * r * (1 - c * c))) * n;
 }

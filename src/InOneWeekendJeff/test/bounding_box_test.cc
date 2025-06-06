6c35d6fc4ad1e4e8b012918e10c7643c254fff5a
Jeff Ahn <ahn.jeff@gmail.com>
2025-06-06 13:36:51 -0700

    move a test into its own method

--- src/InOneWeekendJeff/test/bounding_box_test.cc
+++ src/InOneWeekendJeff/test/bounding_box_test.cc
@@ -12,6 +12,31 @@ class BoundingBoxTest : public TestBase {
     return BoundingBox(Interval(0, 1), Interval(0, 1), Interval(0, 1));
   }
 
+  void test_rays_missing_the_bounding_box() {
+    // Create 3 cases for a ray that is parallel to each axis and does not
+    // hit the bounding box.
+    BoundingBox unit_bb = create_unit_bounding_box();
+    Vec3 direction(3, 0, 0);
+    Point3 origin(-1, 0.5, 0.5);
+    direction = Vec3(-3, 0, 0);
+    Ray incident_ray(origin, direction);
+    assert(!unit_bb.hit(incident_ray, Interval(0, constants::INF_DOUBLE)));
+
+    direction = Vec3(0, 3, 0);
+    incident_ray = Ray(origin, direction);
+    assert(!unit_bb.hit(incident_ray, Interval(0, constants::INF_DOUBLE)));
+    direction = Vec3(0, -3, 0);
+    incident_ray = Ray(origin, direction);
+    assert(!unit_bb.hit(incident_ray, Interval(0, constants::INF_DOUBLE)));
+
+    direction = Vec3(0, 0, 3);
+    incident_ray = Ray(origin, direction);
+    assert(!unit_bb.hit(incident_ray, Interval(0, constants::INF_DOUBLE)));
+    direction = Vec3(0, 0, -3);
+    incident_ray = Ray(origin, direction);
+    assert(!unit_bb.hit(incident_ray, Interval(0, constants::INF_DOUBLE)));
+  }
+
  public:
   void RunTest() override {
     BoundingBox empty_bb;
@@ -35,24 +60,6 @@ class BoundingBoxTest : public TestBase {
     Ray incident_ray(origin, direction);
     assert(unit_bb.hit(incident_ray, Interval(0, constants::INF_DOUBLE)));
 
-    // Create 3 cases for a ray that is parallel to each axis and does not
-    // hit the bounding box.
-    direction = Vec3(-3, 0, 0);
-    incident_ray = Ray(origin, direction);
-    assert(!unit_bb.hit(incident_ray, Interval(0, constants::INF_DOUBLE)));
-
-    direction = Vec3(0, 3, 0);
-    incident_ray = Ray(origin, direction);
-    assert(!unit_bb.hit(incident_ray, Interval(0, constants::INF_DOUBLE)));
-    direction = Vec3(0, -3, 0);
-    incident_ray = Ray(origin, direction);
-    assert(!unit_bb.hit(incident_ray, Interval(0, constants::INF_DOUBLE)));
-
-    direction = Vec3(0, 0, 3);
-    incident_ray = Ray(origin, direction);
-    assert(!unit_bb.hit(incident_ray, Interval(0, constants::INF_DOUBLE)));
-    direction = Vec3(0, 0, -3);
-    incident_ray = Ray(origin, direction);
-    assert(!unit_bb.hit(incident_ray, Interval(0, constants::INF_DOUBLE)));
+    test_rays_missing_the_bounding_box();
   }
 };

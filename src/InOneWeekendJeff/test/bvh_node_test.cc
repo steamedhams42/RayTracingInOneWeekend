dd14db8450149dfb5f44e5aa280a29fa13676cde
Jeff Ahn <ahn.jeff@gmail.com>
2025-06-13 14:04:01 -0700

    add 2 spheres to bvh tree test

--- src/InOneWeekendJeff/test/bvh_node_test.cc
+++ src/InOneWeekendJeff/test/bvh_node_test.cc
@@ -1,14 +1,19 @@
 #include "test_base.h"
 
+#include <cassert>
 #include <memory>
 #include <vector>
 
 #include "InOneWeekendJeff/hittables/bvh_node.h"
+#include "InOneWeekendJeff/hittables/sphere.h"
 
 class BvhNodeTest : public TestBase {
  public:
   void RunTest() override {
     std::vector<std::unique_ptr<Hittable>> hittables;
+    hittables.push_back(std::make_unique<Sphere>(Point3(0, 0, 0), 1));
+    hittables.push_back(std::make_unique<Sphere>(Point3(1, 1, 1), 1));
     BvhNode bvh(hittables, 0, hittables.size());
+    bvh.bounding_box();
   }
 };
